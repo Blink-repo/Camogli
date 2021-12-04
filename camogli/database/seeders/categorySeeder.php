a5b4d219266c41c311d2ef3b4be7c72400beb6a1
Ibrahim Alou Issa <ibrahimalouissa@gmail.com>
2021-12-04 20:34:45 +0100

    feat: category seeder added images

--- camogli/database/seeders/categorySeeder.php
+++ camogli/database/seeders/categorySeeder.php
@@ -14,25 +14,51 @@ class categorySeeder extends Seeder
      */
     public function run()
     {
-        category::create([
+        category::firstOrCreate([
             "name"=>"Pasta's",
             "featured"=>true,
             "active"=>true
 
         ]);
-        category::create([
+        category::firstOrCreate([
             "name"=>"Sauzen",
             "featured"=>true,
             "active"=>true
 
         ]);
-        category::create([
-            "name"=>"Voorbind's",
+        category::firstOrCreate([
+            "name"=>"Carpaccio's",
             "featured"=>true,
             "active"=>true
-
+        ]);
+        category::firstOrCreate([
+            "name"=>"Dolce",
+            "featured"=>false,
+            "active"=>true
+        ]);
+        category::firstOrCreate([
+            "name"=>"Frisdranken",
+            "featured"=>false,
+            "active"=>true
+        ]);
+        category::firstOrCreate([
+            "name"=>"Insalata",
+            "featured"=>false,
+            "active"=>true
+        ]);
+        category::firstOrCreate([
+            "name"=>"Mozzarella",
+            "featured"=>false,
+            "active"=>true
         ]);
 
-        //piemeltjes
+        category::all()->each(function (category $category) {
+            if ($category->media()->count() === 0) {
+                echo "Creating picture for {$category->id}\n";
+                $category
+                    ->addMediaFromUrl('https://source.unsplash.com/random')
+                    ->toMediaCollection('category');
+            }
+        });
     }
 }
