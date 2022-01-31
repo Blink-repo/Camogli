0ad67915a229a669e4d1953d8f22df1d196ca372
Ibrahim Alou Issa <ibrahimalouissa@gmail.com>
2022-01-31 02:21:08 +0100

    fix: downgrade from nuxtjs 3 to 2

--- camogli-front/pages/index.vue
+++ camogli-front/pages/index.vue
@@ -1,5 +1,9 @@
 <template>
-<div class="m-4">
-    <button class="text-lg px-6 py-1 text-white bg-black rounded-full">hello</button>
-</div>
+  <Tutorial />
 </template>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
