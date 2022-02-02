448991f9bf75bdaf1de8854f3f9cea7d76b831fd
Ibrahim Alou Issa <ibrahimalouissa@gmail.com>
2022-02-02 02:42:12 +0100

    feat: food card component

---
+++ camogli-front/components/card.vue
@@ -0,0 +1,20 @@
+<template>
+ <div class="bg-gray-300 p-2 rounded-lg bg-opacity-40 hover:shadow-lg w-48 py-8 m-2">
+   <img src="../assets/Images/pasta.png" alt="" class="w-full object-cover">
+   <h1 class="font-bold text-center text-xl m-2">Pasta ravioli</h1>
+   <div class="grid grid-cols-2 gap-4">
+     <h1 class="font-bold text-left ml-2">$5.90</h1>
+     <i class="bi bi-plus-circle-fill text-right mr-1"></i>
+   </div>
+ </div>
+</template>
+
+<script>
+export default {
+  name: "card"
+}
+</script>
+
+<style scoped>
+
+</style>
--- camogli-front/pages/index.vue
+++ camogli-front/pages/index.vue
@@ -1,17 +1,43 @@
 <template>
   <div class="">
     <navbar/>
+    <h1 class="text-4xl mx-8 m-4 ">Camogli Mechelen <span class="font-bold">Pasta</span> bar <span class="font-bold">Italiaanse</span>  Gerechten</h1>
+    <h1 class="font-bold mx-8 m-2 text-2xl">Food</h1>
+    <div class="flex items-center justify-center">
+      <div class="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4">
+        <div>
+          <card/>
+        </div>
+        <div>
+          <card/>
+        </div>
+        <div>
+          <card/>
+        </div>
+        <div>
+          <card/>
+        </div>
+        <div>
+          <card/>
+        </div>
+        <div>
+          <card/>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import pmbtn from "@/components/pmbtn";
 import navbar from "@/components/navbar";
+import card from "@/components/card";
 export default {
   name: 'IndexPage',
   components:{
     pmbtn,
-    navbar
+    navbar,
+    card
   }
 }
 </script>
