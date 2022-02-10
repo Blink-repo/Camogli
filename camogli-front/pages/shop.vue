30b127508415379f91fd70c91f02d4f868c91e9f
mo <mohammed.haibout@me.com>
2022-02-10 23:16:11 +0100

    feat: shopPage v1

---
+++ camogli-front/pages/shop.vue
@@ -0,0 +1,34 @@
+<template>
+
+<div class="">
+<div class="flex item-center justify-center textcolor-white mt-4">Camogli</div>
+<div v-dragscroll class="flex items-center gap-3 overflow-hidden">
+        <category/>
+        <category/>
+        <category/>
+        <category/>
+        <category/>
+        <category/>
+        <category/>
+        <category/>
+        <category/>
+        <category/>
+        <category/>
+      </div>
+</div>
+</template>
+
+<script>
+import category from "@/components/category";
+import { dragscroll } from 'vue-dragscroll';
+
+export default {
+    name: 'shop',
+    components:{
+        category
+    },
+    directives:{
+        dragscroll
+    }
+}
+</script>
